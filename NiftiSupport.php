36d6b445279b2dd74363e819b7acd3fba4e33565
Antonio Riccelli <anton.riccelli@gmail.com>
2024-10-27 16:43:39 +0000

    update

--- NiftiSupport.php
+++ NiftiSupport.php
@@ -10,68 +10,102 @@
  * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
  */
 
- function create_block_nifti_block_init() {
-    register_block_type( __DIR__ . '/build');
- }
 
- add_action( 'init', 'create_block_nifti_block_init' );
+// The (hopefully) unique prefix 'niisupp' is used to avoid conflicts with other plugins in the WordPress ecosystem.
+// Code inside of the NIISUPP_NiftiSupport class does not require any prefix due to being enclosed within that class.
+// For reference: https://developer.wordpress.org/plugins/plugin-basics/best-practices/
 
-if (!class_exists("NiftiSupport")) {
-
-// NIfTISupport class.
- class NiftiSupport {
-
-    /**
-     * Constructor
-     */
-    function __construct() {
-
-    }
-
-    /**
-     * Add NIfTI (*.nii) as a supported MIME type.
-     * @param mime_types List of existing MIME types.
-     */
-function upload_mime($mime_types) {
-    // add NIfTI (.nii and .nii.gz extensions) to the list of mime types
-    $mime_types['nii'] = 'application/x-nifti';
-    $mime_types['nii.gz'] = 'application/x-nifti';
-    return $mime_types;
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly
 }
 
-// create Papaya HTML
-
-/**
- * Interpret the 'nii' shortcode to insert NIfTI data in posts.
- * @param $atts The attributes of the shortcode.
- * @param $content The content of the shortcode.
- * @return The HTML code to insert in the post.
- */
-function nifti_shortcode($atts, $content = null) {
-    // TODO: add support for the 'nii' shortcode
+function niisupp_create_block_nifti_block_init()
+{
+    register_block_type(__DIR__ . '/build');
 }
 
-/**
- * Enqueue scripts on front end
- */
-function wp_enqueue_scripts() {
-    // TODO: enqueue scripts for frontend
-}
+add_action('init', 'niisupp_create_block_nifti_block_init');
+
+if (!class_exists("NIISUPP_NiftiSupport")) {
+
+    // NIfTISupport class.
+    class NIISUPP_NiftiSupport
+    {
+
+        /**
+         * Constructor
+         */
+        private function __construct()
+        {
+
+            // add NIfTI (*.nii) as a supported MIME type
+            add_filter('upload_mimes', array($this, 'upload_mimes'));
+
+            // add the 'nii' shortcode
+            add_shortcode('nii', array($this, 'nifti_shortcode'));
+
+            // enqueue scripts on the frontend
+            add_action('wp_enqueue_scripts', array($this, 'wp_enqueue_scripts'));
+        }
+
+        /**
+         * Add NIfTI (*.nii) as a supported MIME type.
+         * @param mime_types List of existing MIME types.
+         */
+        function upload_mime($mime_types)
+        {
+            $mime_types['nii'] = 'application/x-nifti';
+            $mime_types['nii.gz'] = 'application/x-nifti';
+            return $mime_types;
+        }
+
+        // create Papaya HTML
+
+        /**
+         * Interpret the 'nii' shortcode to insert NIfTI data in posts.
+         * @param $atts The attributes of the shortcode.
+         * @param $content The content of the shortcode.
+         * @return The HTML code to insert in the post.
+         */
+        function nifti_shortcode($atts, $content = null)
+        {
+            // TODO: add support for the 'nii' shortcode
+        }
+
+        /**
+         * Enqueue scripts on front end
+         */
+        function wp_enqueue_scripts()
+        {
+            $nodeModulesDir = 'node_modules';
+
+            // Papaya Viewer
+            wp_register_script(
+                'papaya-viewer',
+                plugins_url($nodeModulesDir . '/papaya-viewer/release/current/minimal/papaya.js', __FILE__)
+            );
+            wp_register_style(
+                'papaya-viewer',
+                plugins_url(
+                    $nodeModulesDir . '/papaya-viewer/release/current/minimal/papaya.css',
+                    __FILE__
+                )
+            );
+        }
 
 
-/**
- * Use 'nii' shortcode when adding media to blog post
- * @param html The default generated html.
- * @param id The id of the post.
- * @param attachment The post attachment.
- */
-function media_send_to_editor($html, $id, $attachment) {
-    // TODO: use 'nii' shortcode when adding media to blog post
-}
- } // end class NiftiSupport
+        /**
+         * Use 'nii' shortcode when adding media to blog post
+         * @param html The default generated html.
+         * @param id The id of the post.
+         * @param attachment The post attachment.
+         */
+        function media_send_to_editor($html, $id, $attachment)
+        {
+            // TODO: use 'nii' shortcode when adding media to blog post
+        }
+    } // end class NIISUPP_NiftiSupport
 
- // create the NiftiSupport object
- $nifti_support = new NiftiSupport();
-} // end if class_exists("NiftiSupport")
 
- ?>
+    $nifti_support = new NIISUPP_NiftiSupport();
+} // end if class_exists("NIISUPP_NiftiSupport")
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
+# NIfTI Support for WordPress (WORK IN PROGRESS)
+
 <!-- TABLE OF CONTENTS -->
 <details open="open">
   <summary>Table of Contents</summary>
   <ol>
   <li>
-  <a href="#about">About</a>
+  <a href="#description">Description</a>
   </li>
   <li>
   <a href="#technologies">Technologies</a>
@@ -11,12 +13,12 @@
   </ol>
 </details>
 
-<!-- ABOUT -->
-## About
-
-__WORK IN PROGRESS__
+<!-- Description -->
+## Description
 
 Attempt at building a WordPress plugin to allow upload and display of images in [NIfTI](https://en.wikipedia.org/wiki/Neuroimaging_Informatics_Technology_Initiative) format.
+
+I've only recently started dabbling in WordPress due to work requirements, so this is an experiment. Worst case scenario, I will have learned something new.
 
 <!-- TECHNOLOGIES -->
 ## Technologies
@@ -28,7 +30,39 @@ Attempt at building a WordPress plugin to allow upload and display of images in 
 - [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
 - [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
 - [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
-- [Papaya](https://rii-mango.github.io/Papaya/)
+- [Papaya Viewer](https://rii-mango.github.io/Papaya/)
+
+<!-- REQUIREMENTS -->
+## Requirements
+- WordPress 5.0 or higher
+
+## Installation
+
+### Upload
+
+1. Download the plugin zip file
+2. Log into your WordPress site and go to Plugins > Add New
+3. Click Upload Plugin
+4. Select the plugin zip file
+5. Click Install Now
+6. Activate the plugin
+
+### Manual
+
+1. Download the plugin zip file
+2. Unzip the plugin zip file
+3. Copy the plugin folder to your `/wp-content/plugins/` directory
+4. Activate the plugin through the 'Plugins' menu in WordPress
+
+Check out the Codex for more information about [installing plugins manually](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+
+### Git
+
+Using git, browse to your `/wp-content/plugins/` directory and clone this repository:
+
+`git clone git@github.com:Antonio-Riccelli/wordpress-nifti-support.git`
+
+Then go to your Plugins screen and click __Activate__. 
 
 ## License
 
